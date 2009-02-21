ab93c8b82cdbf2c4367825b61d768c67917f78de
Christopher Redinger <redinger@gmail.com>
2009-02-21 15:55:25 -0500

    some error handling

--- tweeple.rb
+++ tweeple.rb
@@ -7,6 +7,27 @@ get '/' do
 end
 
 post '/show' do
-  response = Twitter::Base.new(params[:me], params[:password]).followers.any? {|follower| follower.name == params[:tweeple]} ? 'Yes' : 'No'
-  haml :show, :locals => {:response => response}
+  if params[:me].blank? || params[:password].blank?
+    halt haml :no_credentials
+  end
+
+  user = Twitter::Base.new(params[:me], params[:password])
+  if params[:tweeple].blank?
+    haml :no_follower, :locals => {:follower => user.followers.first}
+  else
+    response = user.followers.any? {|follower| follower.name == params[:tweeple]} ? 'Yes' : 'No'
+    haml :show, :locals => {:response => response}
+  end
+end
+
+error Twitter::RateExceeded do
+  "Twitter says you've been making too many requests. Wait for a bit and try again."
+end
+
+error Twitter::Unavailable do
+  "Twitter is unavailable right now. Try again later."
+end
+
+error Twitter::CantConnect do
+  "Can't connect to twitter. Did you give me the wrong credentials?"
 end
